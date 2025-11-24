2aa8bf9f89fda16c81ff978d9e263675282a3b84
dima <dumitrucepoi03@gmail.com>
2025-11-24 05:56:01 +0000

    code path animation tweak

--- components/code.tsx
+++ components/code.tsx
@@ -23,13 +23,13 @@ const rotateVariants: Variants = {
 
 const caretLeftVariants: Variants = {
   initial: {
-    transform: "translateX(0%) translateY(0%) scale(1)",
+    transform: "translateX(0%) translateY(0%)",
   },
   animate: {
     transform: [
-      "translateX(0%) translateY(0%) scale(1)",
-      "translateX(20%) translateY(0%) scale(1)",
-      "translateX(-100%) translateY(10%) scale(0.9)",
+      "translateX(0%) translateY(0%)",
+      "translateX(20%) translateY(0%)",
+      "translateX(-80%) translateY(10%)",
     ],
     transition: {
       duration: 0.6,
@@ -42,13 +42,13 @@ const caretLeftVariants: Variants = {
 
 const caretRightVariants: Variants = {
   initial: {
-    transform: "translateX(0%) translateY(0%) scale(1)",
+    transform: "translateX(0%) translateY(0%)",
   },
   animate: {
     transform: [
-      "translateX(0%) translateY(0%) scale(1)",
-      "translateX(-20%) translateY(0%) scale(1)",
-      "translateX(100%) translateY(-10%) scale(0.9)",
+      "translateX(0%) translateY(0%)",
+      "translateX(-20%) translateY(0%)",
+      "translateX(80%) translateY(-10%)",
     ],
     transition: {
       duration: 0.6,
@@ -61,13 +61,13 @@ const caretRightVariants: Variants = {
 
 const slashVariants: Variants = {
   initial: {
-    transform: "translateX(0%) translateY(0%) scale(1) rotate(0deg)",
+    transform: "translateX(0%) translateY(0%) rotate(0deg)",
   },
   animate: {
     transform: [
-      "translateX(0%) translateY(0%) scale(1) rotate(0deg)",
-      "translateX(-100%) translateY(0%) scale(1) rotate(5deg)",
-      "translateX(600%) translateY(-10%) scale(0.9) rotate(-30deg)",
+      "translateX(0%) translateY(0%) rotate(0deg)",
+      "translateX(-100%) translateY(0%) rotate(5deg)",
+      "translateX(500%) translateY(-10%) rotate(-30deg)",
     ],
     transition: {
       duration: 0.6,
@@ -81,7 +81,7 @@ const slashVariants: Variants = {
 const codePaths = [
   "M402.625 268.175C402.51 267.54 402.522 266.888 402.659 266.258C402.797 265.627 403.057 265.03 403.425 264.5C403.794 263.97 404.263 263.518 404.806 263.17C405.349 262.821 405.956 262.583 406.591 262.47L409.815 261.89C410.45 261.775 411.101 261.787 411.732 261.924C412.362 262.061 412.96 262.322 413.49 262.69C414.019 263.058 414.471 263.527 414.82 264.071C415.168 264.614 415.406 265.22 415.52 265.856L417.84 278.751C418.07 280.033 417.782 281.355 417.038 282.425C416.295 283.495 415.156 284.225 413.874 284.457L410.65 285.037C409.367 285.267 408.046 284.979 406.976 284.235C405.906 283.491 405.175 282.353 404.944 281.071L402.625 268.175Z",
   "M403.67 266.711C403.326 264.529 404.816 262.481 406.998 262.136L407.851 262.002C410.033 261.658 412.082 263.147 412.426 265.33L414.81 280.443C415.154 282.625 413.664 284.673 411.482 285.017L410.629 285.152C408.447 285.496 406.399 284.006 406.054 281.824L403.67 266.711Z",
-  "M394.314 270.214C393.798 266.941 396.032 263.869 399.306 263.352L416.981 260.564C420.254 260.047 423.326 262.282 423.843 265.555L425.604 276.717C426.12 279.991 423.885 283.063 420.612 283.579L402.936 286.367C399.663 286.884 396.591 284.649 396.075 281.376L394.314 270.214Z",
+  "M396.244 269.906C395.727 266.633 397.962 263.561 401.235 263.045L414.792 260.906C418.065 260.389 421.137 262.624 421.653 265.898L423.414 277.06C423.931 280.333 421.696 283.405 418.423 283.921L404.866 286.06C401.593 286.576 398.521 284.341 398.004 281.068L396.244 269.906Z",
 ];
 
 export function Code() {
